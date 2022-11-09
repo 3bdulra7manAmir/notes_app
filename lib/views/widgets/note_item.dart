422d0d2a5180474318344383c60264e959a8fbe0
3bdulra7manAmir <ethanhunt4d@gmail.com>
2022-11-09 17:05:56 +0200

    uploaded main code to keep it, with some edits

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,61 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
-class NoteItem extends StatelessWidget {
+class NoteItem extends StatelessWidget
+{
   const NoteItem({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
+  Widget build(BuildContext context)
+  {
+    return GestureDetector
+    (
+      onTap: ()
+      {
+        Navigator.push
+        (
           context,
-          MaterialPageRoute(builder: (context) {
+          MaterialPageRoute(builder: (context)
+          {
             return const EditNoteView();
           }),
         );
       },
-      child: Container(
-        decoration: BoxDecoration(
+      child: Container
+      (
+        decoration: BoxDecoration
+        (
           color: const Color(0xffFFCC80),
           borderRadius: BorderRadius.circular(16),
         ),
         padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-        child: Column(
+        child: Column
+        (
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
+          children:
+          [
+            ListTile
+            (
+              title: const Text
+              (
+                'You gotta be kidding with me!',
+                style: TextStyle
+                (
                   fontSize: 26,
                   color: Colors.black,
                 ),
               ),
-              subtitle: Padding(
+              subtitle: Padding
+              (
                 padding: const EdgeInsets.symmetric(vertical: 16),
                 child: Text(
-                  'Build your career with tharwat samy',
-                  style: TextStyle(
+                  'This is Simon Ghost Riley',
+                  style: TextStyle
+                  (
                     fontSize: 18,
                     color: Colors.black.withOpacity(.4),
                   ),
                 ),
               ),
-              trailing: IconButton(
+              trailing: IconButton
+              (
                 onPressed: () {},
-                icon: const Icon(
+                icon: const Icon
+                (
                   Icons.delete,
                   color: Colors.black,
                   size: 30,
                 ),
               ),
             ),
-            Padding(
+            Padding
+            (
               padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'May 21,2022',
-                style: TextStyle(
+              child: Text
+              (
+                'Nov 10,2022',
+                style: TextStyle
+                (
                   color: Colors.black.withOpacity(.4),
                 ),
               ),
