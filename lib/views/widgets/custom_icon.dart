720204ba67d6df33d44e80cd9cffe4949c3f9b70
3bdulra7manAmir <ethanhunt4d@gmail.com>
2022-11-09 16:59:43 +0200

    change icon color and reorginize

--- lib/views/widgets/custom_icon.dart
+++ lib/views/widgets/custom_icon.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 
-class CustomIcon extends StatelessWidget {
+//search icon app bar
+
+class CustomIcon extends StatelessWidget
+{
   const CustomIcon({Key? key, required this.icon}) : super(key: key);
 
   final IconData icon;
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 46,
-      width: 46,
-      decoration: BoxDecoration(
-          color: Colors.white.withOpacity(.05),
-          borderRadius: BorderRadius.circular(16)),
-      child: IconButton(
-        onPressed: () {},
-        icon:  Icon(
+  Widget build(BuildContext context)
+  {
+    return Container
+    (
+      height: 46, //conatiner height
+      width: 46, //container width
+      decoration: BoxDecoration
+      (
+          color: Colors.white.withOpacity(.05), //container color opacity
+          borderRadius: BorderRadius.circular(16)), //border in a circular shape
+      child: IconButton
+      (
+        onPressed: () {}, //do nothing
+        icon:  Icon
+        (
         icon,
-          size: 28,
+          size: 28, //icon size
         ),
       ),
     );
