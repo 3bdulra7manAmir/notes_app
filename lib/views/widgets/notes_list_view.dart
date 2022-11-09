7050f840a4160656be4d4e386ff4c82bd0772a9f
3bdulra7manAmir <ethanhunt4d@gmail.com>
2022-11-09 20:23:59 +0200

    receive color widget

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'note_item.dart';
 
-class NotesListView extends StatelessWidget {
+class NotesListView extends StatelessWidget
+{
   const NotesListView({Key? key}) : super(key: key);
 
+  // final data = const
+  // [
+  //   Color.fromRGBO(100, 200, 255, 0.5),
+  //   Color.fromRGBO(200, 100, 0, 0.5),
+  //   Color.fromRGBO(30, 120, 100, 0.5),
+  // ];
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16),
       child: ListView.builder(
           padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
+          itemBuilder: (context, index)
+          {
             return const Padding(
               padding: EdgeInsets.symmetric(vertical: 4),
               child: NoteItem(),
